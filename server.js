b59c1d8f7c145e27cd82c272876b8909c3c56606
Javokhir Karimov <kjavohir009@gmail.com>
2025-09-19 16:22:25 +0900

    feat:develop author router ejs page

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
-// 1: Kirish codes
+let user = fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+// 1: Kirish codesserver.js
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +35,10 @@ app.set("view engine", "ejs");
 app.post("/creat-item", (req, res) => {
   console.log("req.body");
   res.json({ test: "success" });
+});
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
 });
 
 app.get("/", function (req, res) {
