53b24482519fbfb73230d0e96e1182155a1b777e
Javokhir Karimov <kjavohir009@gmail.com>
2025-09-24 21:13:40 +0900

    feat:MongoDB connection logic

--- server.js
+++ server.js
@@ -1,54 +1,32 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
+const app = require("./app");
+const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
 
-let user = fs.readFile("database/user.json", "utf8", (err, data) => {
-  if (err) {
-    console.log("ERROR:", err);
-  } else {
-    user = JSON.parse(data);
+let db;
+const connectionString =
+  "mongodb+srv://KHAN:BpTpn9yosjSXkLrZ@cluster0.amiiidi.mongodb.net/Reja?retryWrites=true&w=majority&appName=Reja";
+
+mongodb.connect(
+  connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) console.log("Error on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed");
+
+      module.exports = client;
+
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function () {
+        console.log(
+          `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+        );
+      });
+    }
   }
-});
-
-// 1: Kirish codesserver.js
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2 Sessions codes
-
-// 3 Views codes
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing codes
-// app.get("/hello", function (req, res) {
-//   res.end(`<h1 style='background: red'>Hello World by Khan</h1>`);
-// });
-
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1>Siz sovg'alar oynasidasiz</h1>`);
-// });
-
-app.post("/creat-item", (req, res) => {
-  console.log("req.body");
-  res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-  res.render("author", { user: user });
-});
-
-app.get("/", function (req, res) {
-  res.render("reja");
-});
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function () {
-  console.log(
-    `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-});
+);
